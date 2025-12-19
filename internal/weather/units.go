a6d7dc4e218ca7fa22996c8104c3cf3898a618ff
James Vongphasouk <james.vongpha@gmail.com>
2025-12-19 13:58:28 -0700

    feat: failing convert c to f

--- internal/weather/units.go
+++ internal/weather/units.go
@@ -5,3 +5,7 @@ func ConvertFtoC(f float64) float64 {
 	// returns the equivalent farenheit in celsius
 	return (f - 32) * 5/9
 }
+
+func ConvertCToF(f float64) float64 {
+	return 0
+}
--- internal/weather/units_test.go
+++ internal/weather/units_test.go
@@ -5,7 +5,7 @@ import "testing"
 var convertFtoCTests = []struct{
 	name string
 	f 	 float64		//input
-	want float64	//output
+	c float64	//output
 } {
 	{"Freezing", 32.0, 0.0},
 	{"Boiling", 212.0, 100.0},
@@ -16,8 +16,19 @@ func TestConvertFtoC(t *testing.T){
 	for _, tt := range convertFtoCTests {
 		t.Run(tt.name, func(t *testing.T){
 			got := ConvertFtoC(tt.f)
-			if got != tt.want {
-				t.Errorf("got %f, want %f", got, tt.want)
+			if got != tt.c {
+				t.Errorf("got %f, want %f", got, tt.c)
+			}
+		})
+	}
+}
+
+func TestConvertCToF(t *testing.T){
+	for _, tt := range convertFtoCTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ConvertCToF(tt.c)
+			if got != tt.f {
+				t.Errorf("got %f, want %f", got, tt.f)
 			}
 		})
 	}
